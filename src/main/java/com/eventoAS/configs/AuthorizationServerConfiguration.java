ede087d4fd6a42c296946c849405ddc52a5d90c0
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2021-02-22 12:09:30 -0300

    Authorization Server from EventoApp

---
+++ src/main/java/com/eventoAS/configs/AuthorizationServerConfiguration.java
@@ -0,0 +1,87 @@
+package com.eventoAS.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+	@Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+       
+		oauthServer.tokenKeyAccess("isAuthenticated()")
+        	 .checkTokenAccess("isAuthenticated()");
+        
+    }
+    
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+				.withClient("way2learnappclientid")
+				.authorizedGrantTypes("password","authorization_code")
+				.secret(encoder().encode("secret"))
+				.scopes("user_info","read","write")
+				.redirectUris("https://localhost:8443/myapp/login/oauth2/code/way2learnappclient")
+				.autoApprove(true)
+				.and()
+				
+				.withClient("microclient")
+				.authorizedGrantTypes("password","authorization_code","client_credentials")
+				.secret(encoder().encode("secret"))
+				.scopes("user_info", "read")
+				.redirectUris("https://localhost:8443/myapp/login/oauth2/code/way2learnappclient")
+				.autoApprove(true)
+							;
+	}
+	
+	
+	// TODO-4 uncomment the below to inject authentication manager into authorization server endpoints
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		
+		endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore()).accessTokenConverter(accessTokenConverter());
+	}
+	
+
+	 @Bean
+	    public TokenStore tokenStore() {
+	        return new JwtTokenStore(accessTokenConverter());
+	    }
+
+	    @Bean
+	    public JwtAccessTokenConverter accessTokenConverter() {
+	        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+	        final KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(
+	        		new ClassPathResource("eventoas.jks"), "segredo".toCharArray());
+	         converter.setKeyPair(keyStoreKeyFactory.getKeyPair("authserver"));
+	        return converter;
+	    }
+	    
+
+	@Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+}
